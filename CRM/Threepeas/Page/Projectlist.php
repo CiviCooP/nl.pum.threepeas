ee60ba2ea0bb24904d87b57b8d33fdba936ba2eb
Jaap Jansma <info@edeveloper.nl>
2014-05-27 10:17:06 +0200

    renamed hook

--- CRM/Threepeas/Page/Projectlist.php
+++ CRM/Threepeas/Page/Projectlist.php
@@ -111,6 +111,9 @@ class CRM_Threepeas_Page_Projectlist extends CRM_Core_Page {
       $pageActions[] = '<a class="action-item" title="View project details" href="'.$viewUrl.'">View</a>';
       $pageActions[] = '<a class="action-item" title="Edit project" href="'.$editUrl.'">Edit</a>';
       $pageActions[] = '<a class="action-item" title="Drill down project" href="'.$drillUrl.'">Drill Down</a>';
+      //load extra actions from hooks
+      $pageActions = array_merge($pageActions, $this->getProjectActions($project));
+      
       if (isset($project['is_active']) && $project['is_active'] == 1) {
         $pageActions[] = '<a class="action-item" title="Disable project" href="'.$disableUrl.'">Disable</a>';
       } else {
@@ -135,9 +138,9 @@ class CRM_Threepeas_Page_Projectlist extends CRM_Core_Page {
    * @return array with links e.g. array('<a class="action-item" title="my item" href="link.php">link</a>')
    * 
    */
-  protected function getExtraLinks($project) {
+  protected function getProjectActions($project) {
     $hooks = CRM_Utils_Hook::singleton();
-    $return = $hooks->invoke(1, $project, CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, 'civicrm_threepeas_projectlinks');
+    $return = $hooks->invoke(1, $project, CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, CRM_Utils_Hook::$_nullObject, 'civicrm_threepeas_projectactions');
     return $return;
   }
 }
--- doc/hooks.md
+++ doc/hooks.md
@@ -1,16 +1,16 @@
 # Documentation of hooks in the threepeas extension
 
-## hook_civicrm_threepeas_projectlinks
+## hook_civicrm_threepeas_projectactions
 
 This hook is executed when building a projectlist and offers the functionality to add links to additional screens. E.g. a link to linked documents
 
 ### Spec
 
-function hook_civicrm_threepeas_projectlinks($project);
+function hook_civicrm_threepeas_projectactions($project);
 
 ### Parameters
 
-* ~$projects~ array containing the info of the project
+* ~$project~ array containing the info of the project
 
 ### Return value
 
@@ -18,7 +18,7 @@ Returns an array with links
 
 ### Example
 
-    function mymodule_civicrm_threepeas_projectlinks($project) {
+    function mymodule_civicrm_threepeas_projectactions($project) {
         $link = '<a href="civicrm/my_project_links/?pid='.$project['id'].'" title="My project info" class="action-item">My project info"</a>';
         return array($link);
     }
